cb29bbb1b0877e7d075beb34b9c9088d0e32fcab
Eric Luther <ericfelixluther@gmail.com>
2018-04-28 12:50:20 +0200

    admin settings

--- guests/admin.py
+++ guests/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from guests.models import Labels, Guest
 
 # Register your models here.
+
+
+admin.site.register(Labels)
+admin.site.register(Guest)
